393b895c615650db23cf85469f03f8ffd2dd3f72
Maks-Katunin <katunin87@gmail.com>
2021-08-28 17:53:50 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String concatenationWordAndNum;
+        final int NUM = 10;
+        String word = "This num is ";
+        concatenationWordAndNum = word + NUM;
+        System.out.println(concatenationWordAndNum);
+
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (NUM > 0){
+            System.out.println("Вы сохранили положительное число");
+        }else System.out.println("Вы сохранили нуль");
+
+        System.out.println("Введите ваше имя ");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Здравствуйте " + name + " !");
+
+    }
+
+}
